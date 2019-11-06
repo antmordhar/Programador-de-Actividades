287d1f2e561faff33ecd690319e58c3e3863b430
Antonio Morales <antmordhar@correo.ugr.es>
2019-11-06 21:07:46 +0100

    Arrelgado tabulaciones

--- src/ListaActividades.py
+++ src/ListaActividades.py
@@ -4,9 +4,9 @@ class ListaActividades:
    def __init__(self):
        self.activity_list = []
 
-    def add_actividad(self,actividad):
+   def add_actividad(self,actividad):
         self.activity_list.append(actividad)
     
-    def rmv_actividad(self,actividad):
+   def rmv_actividad(self,actividad):
         self.activity_list.remove(actividad)
 
