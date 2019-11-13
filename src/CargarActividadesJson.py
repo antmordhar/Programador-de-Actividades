9ee1219dc2acbc43601fde6003948ae61dab4252
Irene Béjar <irenebejar@correo.ugr.es>
2019-11-13 03:27:35 +0100

    Eliminado main

--- src/CargarActividadesJson.py
+++ src/CargarActividadesJson.py
@@ -26,9 +26,3 @@ class CargarActividadesJson:
         #js = json.dumps(self.activity_list)
         with open(self.my_fichero, 'w') as outfile:
             outfile = json.dump(self.activity_list, outfile)
-        
-    
-if __name__ == "__main__":
-    c = CargarActividadesJson("lista.json")
-    act = Actividad("Prueba","Prueba","11/11/11")
-    c.add(act)
--- src/ListaActividades.py
+++ src/ListaActividades.py
@@ -1,12 +1,16 @@
 #clase que almacena una lista de actividades
+
+from CargarActividadesJson import CargarActividadesJson
+from actividad import Actividad
+
 class ListaActividades: 
    
-   def __init__(self):
-       self.activity_list = []
+     def __init__(self, fichero):
+          self.activity_list = []
+          self.cargador = CargarActividadesJson(fichero)
 
-   def add_actividad(self,actividad):
-        self.activity_list.append(actividad)
+     def add_actividad(self,actividad):
+          self.cargador.add(actividad)
     
-   def rmv_actividad(self,actividad):
-        self.activity_list.remove(actividad)
-
+     def rmv_actividad(self,actividad):
+          self.cargador.rmv(actividad)
